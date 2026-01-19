175dd17a15aba0cf8e1fb19dbedc07f2b1bf64ac
Tongyao Si <tosi@microsoft.com>
2026-01-19 07:02:15 +0000

    apply code review

--- pkg/credentialprovider/azure_credentials.go
+++ pkg/credentialprovider/azure_credentials.go
@@ -43,6 +43,7 @@ const (
 	defaultCacheTTL = 5 * time.Minute
 
 	AcrAudience = "https://containerregistry.azure.net"
+	MCRHost     = "mcr.microsoft.com"
 )
 
 var (
@@ -89,10 +90,10 @@ func NewAcrProvider(req *v1.CredentialProviderRequest, registryMirrorStr string,
 		registryMirror: parseRegistryMirror(registryMirrorStr),
 	}
 
-	targetloginServer, _ := provider.parseACRLoginServerFromImage(req.Image)
-	isACR := (targetloginServer != "")
+	targetloginServer, sourceRegistry := provider.parseACRLoginServerFromImage(req.Image)
+	mcrMirroredToACR := (targetloginServer != "" && strings.EqualFold(sourceRegistry, MCRHost))
 	isNICluster := (len(registryMirrorStr) > 0)
-	shouldUseManagedIdentity := (isACR && isNICluster)
+	shouldUseManagedIdentity := (mcrMirroredToACR && isNICluster)
 
 	var credential azcore.TokenCredential
 	if !shouldUseManagedIdentity && ibConfig.SNIName != "" {
